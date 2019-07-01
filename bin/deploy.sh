c1faac8d69602b9595f52c9758213247a76f0059
Mihael Lovrencic <mihael.lovrencic@wirecard.com>
2019-07-01 09:08:25 +0200

    verbose deploy 3

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+git remote rm origin
+git remote add origin https://miha1:${GITHUB_TOKEN}@github.com/miha1/release-it-test.git
+
 npm i -g npm
 npm install --only=dev
 npx release-it --ci -eV -V
